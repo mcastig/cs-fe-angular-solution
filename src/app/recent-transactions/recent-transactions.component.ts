955ca5bcc4d996febfa6f4fcc5a9ffbc7dfd9a5e
Maximino Castillo <maxcastgarcia@gmail.com>
2021-01-31 02:15:14 -0600

    Add recent transactions markup

--- src/app/recent-transactions/recent-transactions.component.ts
+++ src/app/recent-transactions/recent-transactions.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { HttpClient } from '@angular/common/http';
+import { TRANSACTIONS } from '../mock-transactions';
 
 @Component({
   selector: 'app-recent-transactions',
   templateUrl: './recent-transactions.component.html',
   styleUrls: ['./recent-transactions.component.css']
 })
+@Injectable()
 export class RecentTransactionsComponent implements OnInit {
   faBriefCase = faBriefcase;
+  transactions = TRANSACTIONS;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.transactions;
   }
 
 }
